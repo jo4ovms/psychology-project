70ef5f099cdd71afcd4a37dfef0c15ba2fa39027
jo4ovms <joaovictormacields@gmail.com>
2025-05-01 20:00:30 -0300

    feat: update dependencies and add new modules to the application
    - Added @nestjs/mapped-types and @nestjs/typeorm to dependencies.
    - Included typeorm in dependencies.
    - Imported new modules: UsuarioModule, ClienteModule, EnderecoModule, AgendamentoModule, and ConsultaModule into the AppModule.

--- psychology-api/.eslintrc.js
+++ psychology-api/.eslintrc.js
@@ -5,21 +5,73 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'sonarjs', 'unused-imports'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules', 'coverage'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': 'warn',
+    'no-duplicate-imports': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+
+    'no-throw-literal': 'error',
+    '@typescript-eslint/no-throw-literal': 'error',
+
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'import/no-unresolved': 'error',
+    'unused-imports/no-unused-imports': 'error',
+
+    'sonarjs/cognitive-complexity': ['error', 15],
+    'max-lines-per-function': ['warn', 80],
+    'max-depth': ['warn', 4],
+
+    'eol-last': ['error', 'always'],
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: 'tsconfig.json',
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 };
--- psychology-api/src/app.module.ts
+++ psychology-api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { ClienteModule } from './cliente/cliente.module';
+import { EnderecoModule } from './endereco/endereco.module';
+import { AgendamentoModule } from './agendamento/agendamento.module';
+import { ConsultaModule } from './consulta/consulta.module';
 
 @Module({
-  imports: [],
+  imports: [UsuarioModule, ClienteModule, EnderecoModule, AgendamentoModule, ConsultaModule],
   controllers: [AppController],
   providers: [AppService],
 })
