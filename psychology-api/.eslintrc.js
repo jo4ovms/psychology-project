394b8a60d37f7dae60ac7b6fd5e34933fb62db2c
jo4ovms <joaovictormacields@gmail.com>
2025-05-17 11:23:09 -0300

    chore: update dependencies to NestJS 11 and add new packages for improved functionality

--- psychology-api/.eslintrc.js
+++ psychology-api/.eslintrc.js
@@ -5,15 +5,8 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'sonarjs', 'unused-imports'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'plugin:sonarjs/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
@@ -25,53 +18,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-
     'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-debugger': 'warn',
-    'no-duplicate-imports': 'error',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      },
-    ],
-
-    'no-throw-literal': 'error',
-    '@typescript-eslint/no-throw-literal': 'error',
-
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
-    ],
-    'import/no-unresolved': 'error',
-    'unused-imports/no-unused-imports': 'error',
-
-    'sonarjs/cognitive-complexity': ['error', 15],
-    'max-lines-per-function': ['warn', 80],
-    'max-depth': ['warn', 4],
-
-    'eol-last': ['error', 'always'],
-    'comma-dangle': 'off',
-    '@typescript-eslint/comma-dangle': 'off',
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: 'tsconfig.json',
-      },
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
   },
 };
